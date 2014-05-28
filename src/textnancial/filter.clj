dae13074af087470ebf0612e614b571486c17a99
markxueyuan <ziyaajiang@gmail.com>
2014-05-28 17:35:52 +0800

    filter done

--- src/textnancial/filter.clj
+++ src/textnancial/filter.clj
@@ -5,7 +5,7 @@
             [monger.collection :as mc]
             [monger.multi.collection :as mmc];mirror of mc, but with db as first argument for every function.
             [monger.conversion :refer [from-db-object]]
-            [clojure.string :refer (upper-case)])
+            [textnancial.fetch :as fetch])
   (:import [com.mongodb MongoOptions ServerAddress WriteConcern BasicDBObject BasicDBList]
            org.bson.types.ObjectId
            java.util.ArrayList))
@@ -32,6 +32,12 @@
                 (.close in-file))))]
     (lazy in-file)))
 
+(defn log
+  [file info]
+  (with-open [f-out (io/writer file :append true)]
+    (binding [*out* f-out]
+      (println info))))
+
 (def credit-regex
   #"(?x)
   CREDIT\sAGREEMENT|
@@ -48,7 +54,45 @@
 
 (def table-regex #"TABLE OF CONTENTS")
 
-(remove nil? (seq (doall (for [line (safe-line-seq "D:/data/0000950129-07-001091.txt")]
-  (re-find credit-regex line)))))
+
+(defn match-credit-info
+  [address]
+  (let [doc (str "H:/" address)]
+    (if (.exists (io/file doc))
+      (->> (safe-line-seq doc)
+           (map #(re-find credit-regex %))
+           (zipmap (map str (rest (range))))
+           (remove #(nil? (val %)))
+           (reduce conj {})
+           doall)
+      (log "E:/missing_log.txt" address))))
+
+(defn match-table-of-contents
+  [address]
+  (let [doc (str "H:/" address)]
+    (when (.exists (io/file doc))
+      (->> (safe-line-seq doc)
+           (map #(re-find table-regex %))
+           (zipmap (map str (rest (range))))
+           (remove #(nil? (val %)))
+           (reduce conj {})
+           doall))))
 
 
+;(match-credit-info "data/0000950129-07-001091.txt")
+;(match-table-of-contents "data/0000950129-07-001091.txt")
+(def connection (mg/connect {:host "192.168.1.184" :port 27017}))
+
+(def db (mg/get-db connection "textnancial"))
+
+(defn write-into-mongo
+  [collection entry]
+  (let [address (:url entry)
+        credit (match-credit-info address)
+        table (match-table-of-contents address)
+        result (assoc entry :credit credit :table table)]
+    (mmc/insert db collection result)))
+
+;(write-into-mongo "test" {:url "data/0000950129-07-001091.txt" :haha "hehe"})
+
+(doall (map (partial write-into-mongo "credit_info") (fetch/maps)))
