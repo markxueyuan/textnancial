acced0986f1ceb0c86bad55e5025b522fc4500c0
markxueyuan <ziyaajiang@gmail.com>
2014-05-30 09:43:12 +0800

    hehe

--- src/textnancial/filter.clj
+++ src/textnancial/filter.clj
@@ -5,7 +5,8 @@
             [monger.collection :as mc]
             [monger.multi.collection :as mmc];mirror of mc, but with db as first argument for every function.
             [monger.conversion :refer [from-db-object]]
-            [textnancial.fetch :as fetch])
+            [textnancial.fetch :as fetch]
+            [clojure.string :as string])
   (:import [com.mongodb MongoOptions ServerAddress WriteConcern BasicDBObject BasicDBList]
            org.bson.types.ObjectId
            java.util.ArrayList))
@@ -54,6 +55,7 @@
 
 (def table-regex #"TABLE OF CONTENTS")
 
+(def contract-date-regex #"(?:D|d)ated as of (\D+?)(?:(?:&nbsp;)|\s)(\d+?), (\d{4})")
 
 (defn match-credit-info
   [address]
@@ -78,13 +80,12 @@
            (reduce conj {})
            doall))))
 
-
 ;(match-credit-info "data/0000950129-07-001091.txt")
 ;(match-table-of-contents "data/0000950129-07-001091.txt")
 
-;(def connection (mg/connect {:host "192.168.1.184" :port 27017}))
+(def connection (mg/connect {:host "192.168.1.184" :port 27017}))
 
-;(def db (mg/get-db connection "textnancial"))
+(def db (mg/get-db connection "textnancial"))
 
 (defn write-into-mongo
   [collection entry]
@@ -98,19 +99,81 @@
 
 ;(doall (map (partial write-into-mongo "credit_info") (fetch/maps)))
 
+(defn ignore-nil
+  [func value]
+  (if (nil? value)
+    identity
+    func))
+
 (defn assure
   [max-dist entry]
   (let [t-o-c (->> (:table entry)
                    keys
-                   (map read-string))
+                   (map #((ignore-nil name %) %))
+                   (map #((ignore-nil read-string %) %)))
         line-n (->> (:credit entry)
                     keys
-                    (map read-string))
+                    (map #((ignore-nil name %) %))
+                    (map #((ignore-nil read-string %) %)))
         words (->> (:credit entry)
                    vals)
-        within (fn [n] (some (and (> % n) (<= % (+ n max-dist))) t-o-c))
-        pairs (->> (map vector line-n words)
-                   (filter #(within (first %)))
-                   (sort #(< (first %1) (first %2))))]))
+        within (fn [n] (some #(and (> % n) (<= % (+ n max-dist))) t-o-c))
+        pairs (map vector line-n words)
+        clean (fn [coll] (remove #(nil? (first %)) coll))
+        within-pairs (filter #(within (first %)) (clean pairs))
+        sorted (sort #(< (first %1) (first %2)) (clean pairs))
+        top (first sorted)
+        within-top (when ((ignore-nil within (first top)) (first top))
+                     top)]
+    [within-top within-pairs]))
+
+;(map (partial assure 60) (mmc/find-maps db "credit_info"))
+
+
+
+
+(defn add-result
+  [entry]
+  (let [in-60 (assure 60 entry)
+        in-70 (assure 70 entry)
+        in-80 (assure 80 entry)
+        m1-60 (first in-60)
+        m2-60 (second in-60)
+        m1-70 (first in-70)
+        m2-70 (second in-70)
+        m1-80 (first in-80)
+        m2-80 (second in-80)]
+    (assoc entry :m1-60 m1-60 :m1-70 m1-70 :m2-60 m2-60 :m2-70 m2-70 :m1-80 m1-80 :m2-80 m2-80)))
+
+;(doall (map #(mmc/insert db "credit_result" %) (map add-result (mmc/find-maps db "credit_info_test2"))))
+
+(defn match-contract-date
+  [address]
+  (let [doc (str "H:/" address)]
+    (if (.exists (io/file doc))
+      (let [l-seq (safe-line-seq doc)]
+        (->> (map #(str %1 " " %2) l-seq (rest l-seq))
+             (map #(re-find contract-date-regex %))
+             (map #(and (not (nil? %)) (string/join "-" (rest %))))
+             (zipmap (map str (rest (range))))
+             (filter second)
+             (reduce conj {})
+             doall))
+      (log "E:/missing_log.txt" address))))
+
+;(match-contract-date "edgar/data/764622/0000950123-11-041426.txt")
+
+
+
+(defn add-contract-date
+  [entry]
+  (if-not (empty? (:m2-80 entry))
+    (assoc entry :contract-date (match-contract-date (:url entry)))
+    (assoc entry :contract-date nil)))
+
+
+#_(doseq [a (map add-contract-date (mmc/find-maps db "credit_result"))]
+  (mmc/insert db "credit_date_added" a))
+
 
 
