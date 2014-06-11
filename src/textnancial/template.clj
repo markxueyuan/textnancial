ab3487d3abf500998cf927145b05af827058bdb8
Yuan Xue <ziyaajiang@gmail.com>
2014-06-12 00:06:45 +0800

    more

--- src/textnancial/template.clj
+++ src/textnancial/template.clj
@@ -11,7 +11,9 @@
            org.bson.types.ObjectId
            java.util.ArrayList))
 
-(def connection (mg/connect {:host "192.168.1.184" :port 27017}))
+;(def connection (mg/connect {:host "192.168.1.184" :port 27017}))
+
+(def connection (mg/connect))
 
 (def db (mg/get-db connection "textnancial"))
 
@@ -22,22 +24,54 @@
        (map h/text)
        (map string/trim)))
 
-(def test (first (:file-text (mmc/find-one-as-map db "location_all" {:cikSEC "278166"}))))
+(def test1 (first (:file-text (mmc/find-one-as-map db "location_all" {:cikSEC "278166"}))))
 
-(business-address test)
+(ident-info test1)
 
 (->> (mmc/find-maps db "location_all")
      (map :file-text)
      (map first)
-     (map business-address)
-     (map city))
+     (map ident-info)
+     ;(map zip)
+     )
 
 (defn street
   [address]
   (->> (take (- (count address) 2) address)
        (string/join " ")))
 
+(defn location
+  [address]
+  (-> (first (take-last 2 address))
+      (string/split #"\s+")))
+
 (defn city
   [address]
-(first (take-last 2 address)))
+  (let [loc (location address)]
+    (->> (take (- (count loc) 2) loc)
+         (string/join " "))))
+
+(defn state
+  [address]
+  (first (take-last 2 (location address))))
+
+(defn zip
+  [address]
+  (last (location address)))
+
+(defn business-phone
+  [address]
+  (last address))
+
+(defn ident-info
+  [html]
+  (->> (h/select html [:.identInfo :> h/any-node])
+       (map h/text)
+       ;(map string/trim)
+       identity
+       ))
+
+(defn )
+
+
 
